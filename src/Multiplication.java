2b234dfaca9d1a3a613bd0e07a18238a469b6b00
MuskanVishwakarmaa <muskanvishwakarma06941@gmail.com>
2025-01-17 15:30:46 +0530

    multiply the number

--- src/Multiplication.java
+++ src/Multiplication.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
-
+import java.util.*;
+import java.util.Scanner;
 public class Multiplication {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter 1st number ");
+        int a = sc.nextInt();
+        System.out.println("Enter 2nd number ");
+        int b = sc.nextInt();
+        System.out.println("Enter 3rd number ");
+        int c = sc.nextInt();
+        System.out.println("Result is: "+ (a*b*c));
+    }
 }
